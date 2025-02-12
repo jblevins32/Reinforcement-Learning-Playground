3d143ee8a2999a9d615f120bf5ab6084229e5799
Blevins_Jacob_C_jblevins32 <jblevins32@MEL06988L.me.gatech.edu>
2025-02-12 14:12:37 -0500

    generalized for continuous envs. Trained half cheetah model

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 import torch
-import gym
+import gymnasium as gym
 import os
 from gym.wrappers import RecordVideo
 from globals import root_dir
@@ -18,7 +18,7 @@ env = gym.make(config['gym_model'], render_mode="rgb_array")
 # Recording video parameters
 num_training_episodes = config['epochs']  # total number of training episodes
 video_dir = os.path.join(root_dir, "videos", config['gym_model'], config['rl_alg'])
-env = RecordVideo(env, video_folder=video_dir, name_prefix="training",
+env = RecordVideo(env, video_folder=video_dir, name_prefix="testing",
                     episode_trigger=lambda x: x % config['record_period'] == 0)
 env = RecordEpisodeStatistics(env)
 
@@ -38,18 +38,22 @@ elif config['rl_alg'] =="PPO_CONT":
     rl_alg = PPO_CONT(input_dim=n_obs, output_dim=n_actions, epsilon=config['epsilon'])
 
 # Load the model parameters
-model_dir = os.path.join(root_dir,"models",f"{config['gym_model']}_{config['rl_alg']}.pth")
+model_dir = os.path.join(root_dir,"models",f"{config['gym_model']}_{config['rl_alg']}_2.12313.pth")
 rl_alg.load_state_dict(torch.load(model_dir))
 
 # Run inference and record video
 obs = env.reset()
+obs = obs[0]
 done = False
 while not done:
     with torch.no_grad():
-        mean = rl_alg.policy_mean(torch.tensor(obs, dtype=torch.float32))
+        mean = rl_alg.policy(torch.Tensor(obs))
         std = torch.exp(rl_alg.log_std)
         dist = torch.distributions.Normal(mean, std)
         action = dist.sample().numpy()
 
     obs, reward, done, truncated, _ = env.step(action)
+    print('step taken')
     done = done or truncated
+
+env.close()
